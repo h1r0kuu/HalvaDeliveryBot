17bee19a0492f5fdd4bae5d2c1587220cf61977c
h1r0ku <abhazim.nurbat@gmail.com>
2021-11-19 19:05:33 +0100

    changed_ssl

--- models/db_conf.js
+++ models/db_conf.js
@@ -4,5 +4,6 @@ module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+    ssl: true,
     logging: false
 })
